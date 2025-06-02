f44c8c9f5917480d2f60d55d24113567326cda8b
avinash-9-9 <99avinashverma@gmail.com>
2025-06-02 15:25:38 +0530

    updated styling

--- vite-project/src/Components/Navbar/PrivateNavbar.jsx
+++ vite-project/src/Components/Navbar/PrivateNavbar.jsx
@@ -55,13 +55,13 @@ export default function PrivateNavbar() {
                     to="/add-transaction"
                     className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
                   >
-                    Add Transaction
+                    Transaction
                   </Link>
                   <Link
                     to="/add-category"
                     className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
                   >
-                    Add Category
+                    Category
                   </Link>
                   <Link
                     to="/categories"
